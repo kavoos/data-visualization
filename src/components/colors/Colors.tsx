98d3f86ff5e8853a7a3bb25a22cf24cffebb2b31
Kavoos Boloorchi <kavoos.boloorchi@biomerieux.com>
2021-03-26 16:49:38 +0100

    Refactoring

--- src/components/colors/Colors.tsx
+++ src/components/colors/Colors.tsx
@@ -1,16 +1,17 @@
 import React, { FC, useEffect } from 'react'
 
 export const Colors: FC = () => {
-  const url = 'https://gist.githubusercontent.com/kavoos/6d1874dc4fab9d4ede848d95174a3bea/raw/cssNamedColors.csv'
+  const csvUrl = 'https://gist.githubusercontent.com/kavoos/6d1874dc4fab9d4ede848d95174a3bea/raw/cssNamedColors.csv'
+
+  const fetchText = async (url: string) => {
+    const response = await fetch(url)
+    return await response.text()
+  }
 
   useEffect(() => {
-    fetch(url)
-      .then(response => {
-        response.text()
-          .then(text => {
-            console.log(text)
-          })
-      })
+    fetchText(csvUrl).then(text => {
+      console.log(text)
+    })
   })
 
   return null
--- src/components/face/SmilyFace.tsx
+++ src/components/face/SmilyFace.tsx
@@ -1,28 +1,73 @@
-import React, { FC, useEffect, useRef } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import * as d3 from 'd3'
 import { drawFace } from './face'
 import { drawEye } from './eye'
 import { drawMouse } from './mouse'
 
+interface SmilyFaceConfig {
+  width: number
+  height: number
+  centerX: number
+  centerY: number
+  strokeWidth: number
+  eyeOffsetX: number
+  eyeOffsetY: number
+  eyeRadius: number
+  faceRadius: number
+  mouseWidth: number
+  mouseRadius: number
+}
+
 export const SmilyFace: FC = () => {
+  const svgContainer = useRef<null | HTMLDivElement>(null)
   const svgContext = useRef<null | SVGSVGElement>(null)
 
-  const height = 500
-  const width = 960
-  const centerX = width / 2
-  const centerY = height / 2
-  const strokeWidth = 20
+  const initConfig: SmilyFaceConfig = {
+    width: 500,
+    height: 500,
+    centerX: 250,
+    centerY: 250,
+    strokeWidth: 20,
+    eyeOffsetX: 90,
+    eyeOffsetY: 80,
+    eyeRadius: 50,
+    faceRadius: 240,
+    mouseWidth: 20,
+    mouseRadius: 160
+  }
 
-  const eyeOffsetX = 90
-  const eyeOffsetY = 80
-  const eyeRadius = 50
-
-  const faceRadius = centerY - strokeWidth / 2
-
-  const mouseWidth = 20
-  const mouseRadius = 160
+  const [smilyFaceConfig, setSmilyFaceConfig] = useState(initConfig)
 
   useEffect(() => {
+    if (svgContainer.current) {
+      const w = svgContainer.current.clientWidth
+      const h = svgContainer.current.clientHeight
+
+      setSmilyFaceConfig(prev => {
+        prev.width = w
+        prev.height = h
+        prev.centerX = w / 2
+        prev.centerY = h / 2
+        return prev
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const {
+      width,
+      height,
+      centerX,
+      centerY,
+      strokeWidth,
+      eyeOffsetX,
+      eyeOffsetY,
+      eyeRadius,
+      faceRadius,
+      mouseWidth,
+      mouseRadius
+     } = smilyFaceConfig
+
     const context = d3
       .select(svgContext.current)
       .attr('width', width)
@@ -39,7 +84,7 @@ export const SmilyFace: FC = () => {
   })
 
   return (
-    <div>
+    <div ref={svgContainer} style={{width: '100%', height: '100%'}}>
       <svg ref={svgContext} />
     </div>
   )
